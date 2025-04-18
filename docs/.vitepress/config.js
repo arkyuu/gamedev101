99f01af4ac8a4236f77ec03124e4e21106c8be19
-kyuu <felipefuzi@hotmail.com>
2025-04-18 19:30:44 -0300

    Fix Styles

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,16 +2,19 @@ export default {
   base: '/gamedev101/', // MUITO IMPORTANTE pra GitHub Pages
   title: "GameDev 101",
   description: "Guia para quem quer começar a fazer jogos, por Kyuu",
+  sidebar: {
+    '/': false
+  },
   themeConfig: {
-    sidebar: [
-      { text: 'Introdução', link: '/' },
-      { text: 'Conceitos e Fundamentos', link: '/conceitos' },
-      { text: 'Ferramentas Recomendadas', link: '/ferramentas' },
-      { text: 'Canais e Cursos Gratuitos', link: '/canais' },
-      { text: 'Começando na Prática', link: '/pratica' },
-      { text: 'Recursos Extras', link: '/recursos' },
-      { text: 'Minha Jornada', link: '/kyuu' },
-      { text: 'Dicas do Kyuu', link: '/dicas' }
-    ]
+    // sidebar: [
+    //   { text: 'Introdução', link: '/' },
+    //   { text: 'Conceitos e Fundamentos', link: '/conceitos' },
+    //   { text: 'Ferramentas Recomendadas', link: '/ferramentas' },
+    //   { text: 'Canais e Cursos Gratuitos', link: '/canais' },
+    //   { text: 'Começando na Prática', link: '/pratica' },
+    //   { text: 'Recursos Extras', link: '/recursos' },
+    //   { text: 'Minha Jornada', link: '/kyuu' },
+    //   { text: 'Dicas do Kyuu', link: '/dicas' }
+    // ]
   }
 }
